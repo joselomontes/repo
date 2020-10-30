089e4b3d5a44b329a99b64f1ce7b3b13a7ffaa7e
exit <jmontesdeocam1500@alumno.ipn.mx>
2020-10-30 00:12:19 -0600

    Se modifica tercer renglon

--- ejemplo1/example1.rb
+++ ejemplo1/example1.rb
@@ -1,4 +1,4 @@
 #Creando arreglo
 arrg = [100,200,390,40,1,9,0,18,290,89,50,340,9000,100000,10293,810,4013]
 puts "El valor máximo de la cadena es #{arrg.max} y el mínimo es #{arrg.min} Hola como estas"
-puts "Bien y tu?"
+puts "Bien y tu? My love"
